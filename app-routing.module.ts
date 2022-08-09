1601fed2601abbef13ba132e7d24c29ab794c674
Matheus R <61693870+Math2034@users.noreply.github.com>
2022-08-09 12:12:14 -0300

    Update app-routing.module.ts

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'objetivo-detalhe',
     loadChildren: () => import('./objetivo-detalhe/objetivo-detalhe.module').then( m => m.ObjetivoDetalhePageModule)
   },
